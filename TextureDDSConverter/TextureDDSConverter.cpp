63a79bd5c8665a990e1a1dfa24e4a45a306f3543
newtechnology <abhijeetzala81@gmail.com>
2014-06-19 13:47:09 +0530

    Added Bulk Texture Compressor
    A program to convert bulk of textures in just three click.

--- TextureDDSConverter/TextureDDSConverter.cpp
+++ TextureDDSConverter/TextureDDSConverter.cpp
@@ -1,6 +1,5 @@
 // TextureDDSConverter.cpp : Reads input as texture file paths and converts them to compressed .dds formats
 // Reqiures texconv.exe which comes with directx sdk in the folder of .exe 
-// Not finished
 
 #include "stdafx.h"
 #include <iostream>
@@ -8,12 +7,72 @@
 #include <ostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+std::string removeExtension(std::string filename) {
+    size_t lastdot = filename.find_last_of(".");
+    if (lastdot == std::string::npos) return filename;
+    return filename.substr(0, lastdot); 
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	ifstream ifstr("textures.txt");
+
+	unsigned short numTextures;
+
+
+	bool GenerateMipMaps = false;
+
+	std::string ignore;
+
+	ifstr >> ignore >> numTextures;
+
+	std::vector<std::string> texturepaths;
+
+	printf_s("Generate mip maps?\n");
+
+	char Answer = getchar();
+
+	if (Answer == 'Y' || Answer == 'y')
+	{
+		GenerateMipMaps = true;
+	}
+	else if (Answer == 'N' || Answer == 'n')
+		GenerateMipMaps = false;
+
+	for (unsigned short i = 0; i < numTextures; ++i)
+	{
+		std::string path;
+
+		ifstr >> path;
+
+		printf_s("Reading %s\n", path);
+
+		texturepaths.push_back(path);
+	}
+
+	ofstream output("Textures.bat");
+	
+	output << "@echo off\n";
+	output << "mkdir CompressedTextures\n";
+
+	for (unsigned short i = 0; i < texturepaths.size(); ++i)
+	{
+
+		if (GenerateMipMaps)
+
+            output << "texconv.exe -m 12 -f DXT1 -o CompressedTextures " << texturepaths[i] << "\n";
+		else
+			output << "texconv.exe -f DXT1 -o CompressedTextures " << texturepaths[i] << "\n";
+
+		if (i == texturepaths.size()-1)
+			output << "pause";
+	}
+
+	printf_s("\n\nProcess completed. Run Textures.bat and it'll convert textures to DirectX 9 compressed format.");
 
 	return 0;
 }
---
+++ TextureDDSConverter/Textures.bat
@@ -0,0 +1,24 @@
+@echo off
+mkdir CompressedTextures
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_M_GEN_Foliage01_BranchTest01_N.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_M_GEN_Foliage01_BranchTest01_S.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_M_GEN_Foliage01_Tree_Vines01_D.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_M_GEN_Foliage01_Tree_Vines01_N.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_M_GEN_Foliage01_Tree_Vines01_S.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_raininst_M_GEN_Foliage01_Trees_BarkComplex_MASTER_INST_D.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_raininst_M_GEN_Foliage01_Trees_BarkComplex_MASTER_INST_N.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_raininst_M_GEN_Foliage01_Trees_BarkComplex_MASTER_INST_S.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_RockMasterBig_1Wet_D.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_RockMasterBig_1Wet_N.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_RockMasterBig_1Wet_S.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_RockMesa_06_INST_D.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_RockMesa_06_INST_N.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_RockMesa_06_INST_S.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_StoneBlocks_D.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_StoneBlocks_N.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo_Materials_M_StoneBlocks_S.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo2_Materials_M_Terrain_01_Opt_D.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo2_Materials_M_Terrain_01_Opt_N.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\FoliageDemo2_Materials_M_Terrain_01_Opt_S.bmp
+texconv.exe -f DXT1 -o CompressedTextures C:\Users\i3\Desktop\t\GenericFoliage01_trees_Material_M_GEN_Foliage01_BranchTest01_D.bmp
+pause
